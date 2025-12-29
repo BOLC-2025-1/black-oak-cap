5a184ce798f44efee9b787eb1e426d04abc2b9e4
BOLC-2025-1 <cris.r.lapp@gmail.com>
2025-12-29 11:45:24 -0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Navigation } from '@/components/Navigation';
 import { Hero } from '@/components/Hero';
 import { SocialProof } from '@/components/SocialProof';
@@ -76,5 +78,12 @@ const Index = () => {
     </>
   );
 };
-
-export default Index;
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Index />
+    </React.StrictMode>
+  );
+}
